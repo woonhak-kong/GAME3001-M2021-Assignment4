82cd5e2e48a9b6b6aac058369b54c73977b66a53
WoonHak Kong <woonhak.kong@georgebrown.ca>
2021-08-09 23:04:36 +0900

    CloseCombatCondition is implemented

--- Template/CloseCombatCondition.cpp
+++ Template/CloseCombatCondition.cpp
@@ -1,1 +1,27 @@
 #include "CloseCombatCondition.h"
+
+#include "Util.h"
+#include "Scene.h"
+
+CloseCombatCondition::CloseCombatCondition(Character* character) :
+	ConditionNode(character)
+{
+}
+
+CloseCombatCondition::~CloseCombatCondition()
+{
+}
+
+bool CloseCombatCondition::Condition()
+{
+	glm::vec2 playerPosition = m_pCharacter->getParent()->getPlayer()->getCenterPosition();
+	glm::vec2 selfPosition = m_pCharacter->getCenterPosition();
+	//glm::vec2 direction = playerPosition - selfPosition;
+	float distance = Util::distance(selfPosition, playerPosition);
+
+	if (distance <= 60.f)
+	{
+		return true;
+	}
+	return false;
+}
--- Template/CloseCombatCondition.h
+++ Template/CloseCombatCondition.h
@@ -1,7 +1,13 @@
 #pragma once
+#ifndef __CLOSE_COMBAT_CONDITION__
+#define __CLOSE_COMBAT_CONDITION__
 #include "ConditionNode.h"
-class CloseCombatCondition :
-    public ConditionNode
+class CloseCombatCondition : public ConditionNode
 {
+public:
+	CloseCombatCondition(Character* character);
+	~CloseCombatCondition();
+	bool Condition() override;
 };
 
+#endif // __CLOSE_COMBAT_CONDITION__
